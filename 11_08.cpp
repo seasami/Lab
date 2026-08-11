24b3d96b1fd7195b19993815897092e9b791a0bf
seasami <2256160101@hcmussh.edu.vn>
2026-08-11 20:44:36 +0700

    Save 11_08.cpp

---
+++ 11_08.cpp
@@ -0,0 +1,90 @@
+    #include <bits/stdc++.h>
+using namespace std;
+
+int giaiThua(int n) {
+    int giaiThua = 1;
+    for (int i = 1; i <= n; i++) giaiThua *= i;
+    return giaiThua;
+}
+
+void insertionSort(vector<int> &arr) {
+    int curr;
+    for (int i = 1; i < arr.size(); i++) {
+        curr = arr[i];
+        int j = i - 1;
+        while (j >= 0 && curr < arr[j]) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = curr;
+    }
+}
+
+int main() {
+    cout << "1. Tinh giai thua cua n va kiem tra chan le." << endl;
+    int n;
+    cout << "Nhap n: "; cin >> n;
+    cout << "Giai thua cua n: " << giaiThua(n) << endl;
+    if (giaiThua(n) % 2) cout << "n co giai thua la mot so chan." << endl;
+    else cout << "n co giai thua la mot so chan." << endl;
+
+    cout << "2. Insertion Sort" << endl;
+    int m;
+    cout << "Nhap kich thuoc mang: "; cin >> m;
+    vector<int> a(m);
+    for (int i = 0; i < m; i++) {
+        cout << "Nhap phan tu thu " << i + 1 << ": "; cin >> a[i];
+    }
+
+    insertionSort(a);
+    cout << "Mang duoc sap xep tang dan: ";
+    for (int i = 0; i < m; i++) {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
+    #include <bits/stdc++.h>
+using namespace std;
+
+int giaiThua(int n) {
+    int giaiThua = 1;
+    for (int i = 1; i <= n; i++) giaiThua *= i;
+    return giaiThua;
+}
+
+void insertionSort(vector<int> &arr) {
+    int curr;
+    for (int i = 1; i < arr.size(); i++) {
+        curr = arr[i];
+        int j = i - 1;
+        while (j >= 0 && curr < arr[j]) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = curr;
+    }
+}
+
+int main() {
+    cout << "1. Tinh giai thua cua n va kiem tra chan le." << endl;
+    int n;
+    cout << "Nhap n: "; cin >> n;
+    cout << "Giai thua cua n: " << giaiThua(n) << endl;
+    if (giaiThua(n) % 2) cout << "n co giai thua la mot so chan." << endl;
+    else cout << "n co giai thua la mot so chan." << endl;
+
+    cout << "2. Insertion Sort" << endl;
+    int m;
+    cout << "Nhap kich thuoc mang: "; cin >> m;
+    vector<int> a(m);
+    for (int i = 0; i < m; i++) {
+        cout << "Nhap phan tu thu " << i + 1 << ": "; cin >> a[i];
+    }
+
+    insertionSort(a);
+    cout << "Mang duoc sap xep tang dan: ";
+    for (int i = 0; i < m; i++) {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
